6d9544f5fe4528eecde37ee1bc883bf59c349180
davidao06 <a93274@alunos.uminho.pt>
2022-03-18 12:47:15 +0000

    Fix cilindro

--- PL4/main.cpp
+++ PL4/main.cpp
@@ -13,7 +13,7 @@
 #include <iostream>
 
 
-GLuint buffers[2], indiceCount;
+GLuint verticesB,indicesB, indiceCount;
 
 float alfa = 0.0f, beta = 0.0f, radius = 5.0f;
 float camX, camY, camZ;
@@ -77,72 +77,46 @@ void drawCylinder(float radius,float height,int slices) {
 
 	std::vector<float> vertices;
 	std::vector<GLuint> indices;
-
 	float alpha = 2 * M_PI;
+	vertices.push_back(0);vertices.push_back(0);vertices.push_back(0);
+	vertices.push_back(0);vertices.push_back(height/2);vertices.push_back(0);
+	vertices.push_back(radius*sin(alpha));vertices.push_back(0);vertices.push_back(radius*cos(alpha));
+	vertices.push_back(radius*sin(alpha));vertices.push_back(height/2);vertices.push_back(radius*cos(alpha));
+	int index = 4;
+
 	float sliceStep = alpha/slices;
-	for (int i = 0; i <= slices;i++) {
+	for (int i = 0; i < slices;i++) {
 		float alphaAngle = alpha - i * sliceStep;
-		float x = radius * sin(alphaAngle);
-		float z = radius * cos(alphaAngle);
-		Coordenada c;
-		c.x = x;
-		c.z = z;
-		coords.push_back(c);
-	}
-		vertices.push_back(0);vertices.push_back(0);vertices.push_back(0);
-		vertices.push_back(0);vertices.push_back(height);vertices.push_back(0);
-		int index = 2;
-
-	for (int i = 0;i < coords.size() - 1;i++) {
-		Coordenada c1 = coords[i],c2 = coords[i+1];
-		int indexC1= 0;
-		int indexC2 = 0;
-		int indexC3 = 0;
-		int indexC4 = 0;
-		if ((indexC1 = findVerticeInVector(vertices,c1.x,0,c1.z)) == -1) {
-			vertices.push_back(c1.x);vertices.push_back(0);vertices.push_back(c1.z);
-			indices.push_back(index);
-			indexC1 = index;
-			index++;
-		}
-		else indices.push_back(indexC1);
-
-		if ((indexC2 = findVerticeInVector(vertices,c2.x,0,c2.z)) == -1) {
-			vertices.push_back(c2.x);vertices.push_back(0);vertices.push_back(c2.z);
-			indices.push_back(index);
-			indexC2 = index;
-			index++;
-		}
-		else indices.push_back(indexC2);
+		float alphaAngle2 = alpha - (i+1) * sliceStep;
+		float x2 = radius * sin(alphaAngle2);
+		float z2 = radius * cos(alphaAngle2);
+		indices.push_back(index-2);
+		vertices.push_back(x2);
+		vertices.push_back(0);
+		vertices.push_back(z2);
+		indices.push_back(index);
 		indices.push_back(0);
 
-		if ((indexC4 = findVerticeInVector(vertices,c2.x,height,c2.z)) == -1) {
-			vertices.push_back(c2.x);vertices.push_back(height);vertices.push_back(c2.z);
-			indices.push_back(index);
-			indexC4 = index;
-			index++;
-		}
-		else indices.push_back(indexC4);
-
-		if ((indexC3 = findVerticeInVector(vertices,c1.x,height,c1.z)) == -1) {
-			vertices.push_back(c1.x);vertices.push_back(height);vertices.push_back(c1.z);
-			indices.push_back(index);
-			indexC3 = index;
-			index++;
-		}
-		else indices.push_back(indexC3);
+		indices.push_back(index-1);
 		indices.push_back(1);
+		vertices.push_back(x2);
+		vertices.push_back(height/2);
+		vertices.push_back(z2);
+		indices.push_back(index+1);
 
-		indices.push_back(indexC4);
-		indices.push_back(indexC2);
-		indices.push_back(indexC1);
+		indices.push_back(index-2);
+		indices.push_back(index-1);
+		indices.push_back(index+1);
 
-		indices.push_back(indexC3);
-		indices.push_back(indexC4);
-		indices.push_back(indexC1);
+		indices.push_back(index+1);
+		indices.push_back(index);
+		indices.push_back(index-2);
+
+		index +=2;
 	}
 
-	glBindBuffer(GL_ARRAY_BUFFER,buffers[0]);
+	glGenBuffers(1,&verticesB);
+	glBindBuffer(GL_ARRAY_BUFFER,verticesB);
 
 	//copiar o vector para o buffer
 	glBufferData(
@@ -152,7 +126,8 @@ void drawCylinder(float radius,float height,int slices) {
 		GL_STATIC_DRAW
 	);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,buffers[1]);
+	glGenBuffers(1,&indicesB);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,indicesB);
 
 	glBufferData(
 		GL_ELEMENT_ARRAY_BUFFER,
@@ -167,9 +142,8 @@ void drawCylinder(float radius,float height,int slices) {
 void prepareData() {
 
 	//Criar o VBO
-	glGenBuffers(2,buffers);
 	
-	drawCylinder(1,2,10000);
+	drawCylinder(1,2,1000);
 }
 
 void renderScene(void) {
@@ -183,10 +157,10 @@ void renderScene(void) {
 		0.0, 0.0, 0.0,
 		0.0f, 1.0f, 0.0f);
 
-	glBindBuffer(GL_ARRAY_BUFFER,buffers[0]);
+	glBindBuffer(GL_ARRAY_BUFFER,verticesB);
 	glVertexPointer(3,GL_FLOAT,0,0);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,buffers[1]);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,indicesB);
 	glDrawElements(
 		GL_TRIANGLES,
 		indiceCount,
@@ -287,7 +261,6 @@ int main(int argc, char **argv) {
 #ifndef __APPLE__
 	glewInit();
 #endif
-	prepareData();
 
 
 //  OpenGL settings
@@ -300,6 +273,7 @@ int main(int argc, char **argv) {
 
 	printInfo();
 
+	prepareData();
 // enter GLUT's main cycle
 	glutMainLoop();
 	
